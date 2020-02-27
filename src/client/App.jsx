c7b5b94b3bf30e98dad05a04438288990066f113
benjacoblee <benjacoblee@gmail.com>
2020-02-27 16:32:11 +0800

    two handlers, one for input, one for button

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React from "react";
+import { hot } from "react-hot-loader";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentInput: "",
+      todos: []
+    };
+  }
+
+  handleInputField(event) {
+    this.setState({ currentInput: event.target.value });
+    console.log(this.state.currentInput);
+  }
+
+  addTodo() {
+    this.state.todos.push(this.state.currentInput);
+    console.log(this.state.todos)
+  }
+
   render() {
     return (
       <div>
-        Welcome.
+        <input
+          onChange={event => {
+            this.handleInputField(event);
+          }}
+        ></input>
+        <button
+          onClick={event => {
+            this.addTodo(event);
+          }}
+        >
+          Add todo
+        </button>
       </div>
     );
   }
